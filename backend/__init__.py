5e677932b0add93bed95f42b5933da66befd6fdd
Danny Khorasanizadeh <dannykhorasanizadeh@ec.iut.ac.ir>
2022-06-04 21:29:52 +0430

    Add label and instruction table to assembler

--- backend/__init__.py
+++
@@ -1,2 +0,0 @@
-#To-Do: 1. Ram module
-#       2. REST API
--- backend/assembler.py
+++ backend/assembler.py
@@ -8,7 +8,7 @@ def int2hex(number:int, bits:int):
 
 class Assembler:
     regex = re.compile(
-        "([\\w\\d]{0,3}(?=,))?,?[^\\S\\r\\n]?(\\w{3})[^\\S\\r\\n]?([-\\w\\d]+)?[^\\S\\r\\n]?(I)?")
+        "([\\w\\d]{0,3}(?=,))?,?[^\\S\\r\\n]?(\\w{3})[^\\S\\r\\n]?([-\\w\\d]+)?[^\\S\\r\\n]?(I)?[^\\S\\r\\n]?(\\/\\w+)?")
     pseudo_instructions = ['ORG', 'END', 'DEC', 'HEX']
     memory_reference_instructions = {
         'AND': '000',
@@ -41,28 +41,36 @@ class Assembler:
     }
 
     def __init__(self, code: List[str]) -> None:
-        self.code = code
+        self.code = [line.upper() for line in code]
         self.symbol_table: Dict[str, int] = {}
-        self.data: List[bytearray] = [bytearray(2) for i in range(2048)]
+        self.instruction_table = {}
+        self.label_table = {}
+        self.data: List[bytearray] = [bytearray(2) for _ in range(2048)]
 
     def assemble(self) -> None:
         self.first_pass()
         self.second_pass()
-        self.save_binary()
 
     def first_pass(self) -> None:
         line_counter = 0
+        file_line = 0
         for line in self.code:
             match = self.regex.match(line)
+            file_line += 1
             if match:
                 if match[1]:
                     self.symbol_table[match[1]] = line_counter
+                if match[2] not in ['ORG','END']:
+                    self.instruction_table[line_counter] = f'{str(match[2] or "")} {str(match[3] or "")} {str(match[4] or "")}'.strip()
                 elif match[2] == 'ORG':
                     line_counter = int(match[3],16)
                     continue
                 elif match[2] == 'END':
                     break
                 line_counter += 1
+            else:
+                raise SyntaxError(f'Parsing error on line {file_line}')
+        self.label_table = dict((v, k) for k, v in self.symbol_table.items())
 
     def second_pass(self) -> None:
         line_counter = 0
@@ -90,7 +98,6 @@ class Assembler:
                         instruction = '1' + instruction
                     else:
                         instruction = '0' + instruction
-                    print(instruction)
                     self.data[line_counter] = bytearray.fromhex(
                         int2hex(int(instruction, 2),16)[2:])
                 line_counter += 1
