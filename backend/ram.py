85d720ccef09a40a21e819a2fea854a0fd71f2f3
Danny Khorasanizadeh <dannykhorasanizadeh@ec.iut.ac.ir>
2022-06-04 21:30:24 +0430

    ram serialize and load methdos

--- backend/ram.py
+++ backend/ram.py
@@ -1,7 +1,10 @@
+import io
 class Word:
     def __init__(self) -> None:
         self.size = 16
         self.data = [0] * 16
+        self.label = ""
+        self.instruction = ""
 
     def __getitem__(self, item):
         return self.data[item]
@@ -12,6 +15,8 @@ class Word:
     def __ilshift__(self, other):
         for i in range(self.size):
             self.data[i] = other.data[i]
+        self.instruction = ""
+        return self
 
     def __imod__(self, value):
         value = int(value)
@@ -20,6 +25,8 @@ class Word:
 
     def __imatmul__(self, value):
         value = int(value, 16)
+        if value >= 2**(self.size-1):
+            value -= 2**self.size
         self.set_value(value)
         return self
 
@@ -27,20 +34,24 @@ class Word:
         if -32768 <= value <= 32767:
             if value < 0:
                 self.data = list(
-                    map(int, list(bin(-value))[2:].zfill(self.size)))[-self.size:]
-                self.complement()
-                self.increment()
+                    map(int, list(bin(value & (2**self.size - 1))[2:])))[-self.size:]
             else:
                 self.data = list(
-                    map(int, list(bin(value))[2:].zfill(self.size)))[-self.size:]
+                    map(int, list(bin(value)[2:].zfill(self.size))))[-self.size:]
 
     def __int__(self):
         result = 0
-        for bit in self.data[1:].reverse():
+        for bit in self.data[1:]:
             result = (result << 1) | bit
         if self.data[0]:
             result -= 32768
         return result
+
+    def hex(self):
+        result = 0
+        for bit in self.data:
+            result = (result << 1) | bit
+        return hex(result)
 
     def increment(self):
         self %= int(self)+1
@@ -55,12 +66,37 @@ class Ram:
         self.words = [Word() for _ in range(2048)]
 
     def __getitem__(self, item):
-        return self.data[item]
+        return self.words[item]
 
     def __setitem__(self, item, value):
-        self.data[item] = value
+        self.words[item] = value
 
     def load_bin(self, file_name):
-        with open(file_name) as bin_file:
+        with open(file_name, 'rb') as bin_file:
             for word in self.words:
-                word @= bin_file.read(2).encode('utf-8').hex()
+                word @= bin_file.read(2).hex()
+    
+    def load_bytearray(self, bytearray):
+        bin_file = io.BytesIO(bytearray)
+        for word in self.words:
+                word @= bin_file.read(2).hex()
+
+    def load_label_table(self, table):
+        for line,label in table.items():
+            self.words[line].label = label
+
+    def load_instruction_table(self,table):
+        for line,instruction in table.items():
+            self.words[line].instruction = instruction
+
+    def json(self):
+        words = []
+        line_number = 0
+        for word in self.words:
+            words.append({
+                "address": hex(line_number)[2:],
+                "value": word.hex(),
+                "label": word.label,
+                "instruction": word.instruction})
+            line_number += 1
+        return words
