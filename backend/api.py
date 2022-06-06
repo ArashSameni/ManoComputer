ac24c713dc8b529ba36ef8dd2e4d31bc6b04c77b
Arash Sameni <arashsameniy@gmail.com>
2022-06-06 19:35:48 +0430

    Fix flask cors

--- backend/api.py
+++ backend/api.py
@@ -1,9 +1,11 @@
 import json
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 from .computer import Computer
 from .assembler import Assembler
 
 app = Flask(__name__)
+CORS(app)
 c = Computer()
 
 @app.route('/clock', methods=['GET'])
